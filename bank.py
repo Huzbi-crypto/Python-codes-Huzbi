8862a7b5556a195ed8dd4d9409b6dd6af47f6206
Huzbi-crypto <76440095+Huzbi-crypto@users.noreply.github.com>
2022-06-08 15:16:27 +0000

    Update log# 080622.0814.1
    Made the code a bit cleaner now thanks to the help from fellow coders!

--- bank.py
+++ bank.py
@@ -1,15 +1,15 @@
 for _ in range(16):
 
     # Let the user input the greeting
-    greetings = input("Greeting: ")
+    greetings = input("Greeting: ").lower()
 
 
     # Check to see if the greeting input is Hello, give 0$ if so
-    if greetings == ("Hello") or greetings == ("hello") or greetings == ("Hello there") or greetings == ("hello there") or greetings == ("Hello there, Huzbi") or greetings == ("hello there, Huzbi") :
+    if greetings in ("hello"):
         print("0$")
 
     # Check to see if the greeting starts with 'h' but isn't 'hello' and give 20$    
-    elif greetings == ("Hey") or greetings == ("Hi") or greetings == ("How's it going") or greetings == ("hey") or greetings == ("hi") or greetings == ("how's it going") or greetings == ("how are ya") or greetings == ("How are ya") or greetings == ("How are you") or greetings == ("how are you") or greetings == ("How do you do") or greetings == ("how do you do"):
+    elif greetings in ("hey", "how's it going", "how are ya", "how are you", "how do you do", "hi"):
         print ("20$")
 
     # If the greeting is something else or blank then give 100$    

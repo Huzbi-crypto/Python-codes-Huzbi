a7e58393d8d6930ff7af9a8313818958a669c49d
Huzbi <76440095+Huzbi-crypto@users.noreply.github.com>
2023-06-30 15:29:37 +0500

    Update converter.py
    Added the batch conversion functionality for WebP to PNG/JPG type.

--- Image Converter/converter.py
+++ Image Converter/converter.py
@@ -1,29 +1,34 @@
 # This is a basic code which will convert a format of an image file into another format.
 # Author: Huzbi
-# Date Created: 30/06/2023
-# Latest Update: 30/06/2023
+# Date: 30/06/2023
+# Last Update: 30/06/2023 3:27PM PST
+
 from PIL import Image
+import os
 
 def webp_to_img(input_path):
     print("To PNG or JPEG? Enter in small caps.")
     ft_type = input()
     if (ft_type == "png"):
-        try:
-            im = Image.open(input_path).convert("RGB")
-            output_path = "file.png"
-            im.save(output_path, "png")
-            print("Conversion successful!")
-        except IOError:
-            print("Cannot convert the image.")
+        for filename in os.listdir(input_path):
+            if filename.endswith('.webp'):
+                file_path = os.path.join(input_path, filename)
+                im = Image.open(file_path).convert("RGB")
+                new_file_path = os.path.splitext(file_path)[0] + '.png'
+                im.save(new_file_path, "png")
+                print("Conversion successful!")
     elif (ft_type == "jpeg" or ft_type == "jpg"):
-        try:
-            im = Image.open(input_path).convert("RGB")
-            output_path = "file.jpeg"
-            im.save(output_path, "jpeg")
-            print("Conversion successful!")
-        except IOError:
-            print("Cannot convert the image.")
+        for filename in os.listdir(input_path):
+            if filename.endswith('.webp'):
+                file_path = os.path.join(input_path, filename)
+                im = Image.open(file_path).convert("RGB")
+                new_file_path = os.path.splitext(file_path)[0] + '.jpg'
+                im.save(new_file_path, "jpeg")
+                print("Conversion Successful!")
+    else:
+        print("Error: Conversion Unsuccessful.")
 
-input_path = "file.webp"
+script_directory = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(script_directory, "webp_files")
 
 webp_to_img(input_path)
